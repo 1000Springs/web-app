c731a2dd8c671a749ec3a548f93616f4bae52300
Mathew Button <mathewdbutton@gmail.com>
2013-08-29 22:31:37 +1200

    changed sql uri to be a config argument

--- hotspringsapp/__init__.py
+++ hotspringsapp/__init__.py
@@ -1,13 +1,8 @@
 from flask import Flask
 
-
 app = Flask('hotspringsapp')
 
 app.config.from_pyfile('hotsprings.cfg')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:admin@localhost/geothermaldb'
 
 import hotspringsapp.views
 import hotspringsapp.models
-
-
-
