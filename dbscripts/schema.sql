51d7a59281975dd431bf7901ab8de225def71d11
Mathew Button <mathewdbutton@gmail.com>
2013-09-15 15:50:56 +1200

    extra fields added to tables

--- dbscripts/schema.sql
+++ dbscripts/schema.sql
@@ -1,60 +1,63 @@
 
-CREATE TABLE `images` (
-  `image_id` int(11) NOT NULL,
-  `sample_id` int(11) DEFAULT NULL,
-  `image_path` text NOT NULL,
-  `image_name` varchar(50) NOT NULL DEFAULT '',
-  PRIMARY KEY (`image_id`)
-); 
-
-
-
 CREATE TABLE `location` (
-  `FEATURE_NC` varchar(100) NOT NULL DEFAULT '',
-  `COMMON_FEATURE_NAME` text,
-  `eastings` double DEFAULT NULL,
-  `northings` double DEFAULT NULL,
-  `FEATURE_SYSTEM` text,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `feature_name` varchar(100) DEFAULT NULL,
+  `lat` double DEFAULT NULL,
+  `lng` double DEFAULT NULL,
+  `feature_system` text,
   `description` text,
   `toilet` tinyint(1) DEFAULT NULL,
   `parkbench` tinyint(1) DEFAULT NULL,
   `track` tinyint(1) DEFAULT NULL,
   `private` tinyint(1) DEFAULT NULL,
   `colour` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`FEATURE_NC`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `feature_name` (`feature_name`)
 );
 
-
-
 CREATE TABLE `physical_data` (
-  `SAMPLE_ID` int(11) NOT NULL DEFAULT '0',
-  `PHYS_ID` int(11) NOT NULL DEFAULT '0',
-  `TEMPERATURE` float DEFAULT NULL,
-  `PH_LEVEL` float DEFAULT NULL,
-  `REDOX` float DEFAULT NULL,
-  `DISSOLVED_OXYGEN` float DEFAULT NULL,
-  `CONDUCTIVITY` float DEFAULT NULL,
-  `DATE_GATHERED` date DEFAULT NULL,
-  PRIMARY KEY (`SAMPLE_ID`,`PHYS_ID`),
-  CONSTRAINT `FK_PHYSICAL_SAMPLE_ID` FOREIGN KEY (`SAMPLE_ID`) REFERENCES `sample` (`SAMPLE_ID`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `initialTemp` double DEFAULT NULL,
+  `pH` double DEFAULT NULL,
+  `redox` double DEFAULT NULL,
+  `dO` double DEFAULT NULL,
+  `conductivity` double DEFAULT NULL,
+  `date_gathered` date DEFAULT NULL,
+  `size` varchar(20) DEFAULT NULL,
+  `colour` varchar(50) DEFAULT NULL,
+  `ebullition` varchar(50) DEFAULT NULL,
+  `turbidity` double DEFAULT NULL,
+  `dnaVolume` double DEFAULT NULL,
+  `ferrousIronAbs` double DEFAULT NULL,
+  `sampler` varchar(100) DEFAULT NULL,
+  `sampleTemp` double DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `sample` (
-  `SAMPLE_ID` int(11) NOT NULL DEFAULT '0',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `date_gathered` date DEFAULT NULL,
-  `LOCATION_FEATURE_NC` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`SAMPLE_ID`),
-  KEY `FK_SAMPLE_FEATURE_NC` (`LOCATION_FEATURE_NC`),
-  CONSTRAINT `FK_SAMPLE_FEATURE_NC` FOREIGN KEY (`LOCATION_FEATURE_NC`) REFERENCES `location` (`FEATURE_NC`)
+  `location_id` int(11) NOT NULL,
+  `phys_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_SAMPLE_location` (`location_id`),
+  KEY `FK_SAMPLE_phys` (`phys_id`),
+  CONSTRAINT `FK_SAMPLE_location` FOREIGN KEY (`location_id`) REFERENCES `location` (`id`),
+  CONSTRAINT `FK_SAMPLE_phys` FOREIGN KEY (`phys_id`) REFERENCES `physical_data` (`id`)
 );
 
-
-
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sample_id` int(11) DEFAULT NULL,
+  `image_path` text NOT NULL,
+  `image_name` varchar(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `FK_images_samples` (`sample_id`),
+  CONSTRAINT `FK_images_samples` FOREIGN KEY (`sample_id`) REFERENCES `sample` (`id`)
+);
 
 CREATE TABLE `user` (
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   PRIMARY KEY (`username`)
 ) ;
-
-
