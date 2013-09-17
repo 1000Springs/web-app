f9ff9bf0077bacb24be9429685c405f59a5e9871
Mathew Button <mathewdbutton@gmail.com>
2013-09-17 12:01:17 +1200

    cleaned up old database connection code

--- hotspringsapp/views.py
+++ hotspringsapp/views.py
@@ -8,8 +8,6 @@ from models import *
 from forms import *
 
 
-
-
 def get_user_name():
     """ Fetch the logged in User"""
     return session['logged_in'] if session.has_key('logged_in') else None
@@ -47,26 +45,12 @@ def about():
 def browseby():
 	return render_template('browseby.html');
 
-
-#Old database Connection
-@app.before_request
-def before_request():
-	g.db = connect_db()
-
-@app.teardown_request
-def teardown_request(exception):
-    g.db.close()
-	
-def connect_db():
-	return MySQLdb.connect(host="localhost",user="root",passwd="admin",db="geothermaldb")
-#	
 @app.route('/')
 def index():
 	return render_template('index.html')
 
 @app.route('/search')
 def search():
-
 	
 	return redirect(url_for('login')) 
 
@@ -80,90 +64,88 @@ def simplesearch():
 
 	return render_template('simplesearch.html',form=form)
 	
-@app.route('/results')
-def results():
+# @app.route('/results')
+# def results():
 	
 	
 
-	cur = g.db.cursor()
+# 	cur = g.db.cursor()
 	
-	#e.g. [u'temperature,=,50', u'redox,=,60', u'dissolved_oxygen,=,90']
-	conditions = request.args.getlist("conditions")
+# 	#e.g. [u'temperature,=,50', u'redox,=,60', u'dissolved_oxygen,=,90']
+# 	conditions = request.args.getlist("conditions")
 
-	#e.g. [[u'temperature', u'=', u'50'], [u'redox', u'=', u'60'], [u'dissolved_oxygen', u'=', u'90']]
-	conditions = [x.split(',') for x in conditions]
+# 	#e.g. [[u'temperature', u'=', u'50'], [u'redox', u'=', u'60'], [u'dissolved_oxygen', u'=', u'90']]
+# 	conditions = [x.split(',') for x in conditions]
 
-	#e.g. [u'temperature = 50', u'redox = 60', u'dissolved_oxygen = 90']
-	conditions = [" ".join(x) for x in conditions]
+# 	#e.g. [u'temperature = 50', u'redox = 60', u'dissolved_oxygen = 90']
+# 	conditions = [" ".join(x) for x in conditions]
 
-	query = "Select * FROM physical_data where "
+# 	query = "Select * FROM physical_data where "
 
-	#adds conditions onto end of query e.g. "temperature = 50 and"
-	for cond in conditions[:-1]:
-		query += (cond + " and ")
+# 	#adds conditions onto end of query e.g. "temperature = 50 and"
+# 	for cond in conditions[:-1]:
+# 		query += (cond + " and ")
 
-	#for the last item so that another "and" does not get added
-	query += conditions[-1]
+# 	#for the last item so that another "and" does not get added
+# 	query += conditions[-1]
 
-	cur.execute(query);	
+# 	cur.execute(query);	
 	
-	entries = [dict(sample_id=row[0], phys_id=row[1], temperature=row[2], ph_level=row[3], redox=row[4], dissolved_oxygen=row[5], conductivity=row[6] ) for row in cur.fetchall()]
+# 	entries = [dict(sample_id=row[0], phys_id=row[1], temperature=row[2], ph_level=row[3], redox=row[4], dissolved_oxygen=row[5], conductivity=row[6] ) for row in cur.fetchall()]
 	
-	cur.close()
+# 	cur.close()
 	
-	return render_template('results.html', entries=entries)
+# 	return render_template('results.html', entries=entries)
 
-@app.route('/mapresults')
-def mapResults():
+# @app.route('/mapresults')
+# def mapResults():
 
-	cur = g.db.cursor()
+# 	cur = g.db.cursor()
 
-	sampleSites = request.args.getlist('sampleSite');
+# 	sampleSites = request.args.getlist('sampleSite');
 	
-	sampleSites = "(" + ','.join(["'"+x+"'" for x in sampleSites]) + ")"
+# 	sampleSites = "(" + ','.join(["'"+x+"'" for x in sampleSites]) + ")"
 
-	query = """Select p.sample_id,p.phys_id,p.initialTemp,p.ph_level,p.redox,p.dissolved_oxygen,p.conductivity 
-			   from physical_data p, location l, sample s 
-			   where s.sample_id = p.sample_id 
-			   and s.location_feature_nc = l.feature_nc 
-			   and l.feature_nc in {selectedSites}""".format(selectedSites=sampleSites)
+# 	query = """Select p.sample_id,p.phys_id,p.initialTemp,p.ph_level,p.redox,p.dissolved_oxygen,p.conductivity 
+# 			   from physical_data p, location l, sample s 
+# 			   where s.sample_id = p.sample_id 
+# 			   and s.location_feature_nc = l.feature_nc 
+# 			   and l.feature_nc in {selectedSites}""".format(selectedSites=sampleSites)
 
-	cur.execute(query)
+# 	cur.execute(query)
 
-	entries = [dict(sample_id=row[0], phys_id=row[1], temperature=row[2], ph_level=row[3], redox=row[4], dissolved_oxygen=row[5], conductivity=row[6] ) for row in cur.fetchall()] 
+# 	entries = [dict(sample_id=row[0], phys_id=row[1], temperature=row[2], ph_level=row[3], redox=row[4], dissolved_oxygen=row[5], conductivity=row[6] ) for row in cur.fetchall()] 
 
-	cur.close();
+# 	cur.close();
 
-	return render_template('results.html',entries=entries)
+# 	return render_template('results.html',entries=entries)
 	
-@app.route('/mapsearch')
-def mapsearch():
+# @app.route('/mapsearch')
+# def mapsearch():
 
-	cur = g.db.cursor()
+# 	cur = g.db.cursor()
 	
-	cur.execute("""select s.location_feature_nc,l.common_feature_name,l.feature_system,l.description,p.initialTemp, l.lat,l.lng
-				from sample s, physical_data p, location l 
-				where l.FEATURE_NC = s.location_feature_nc
-				and p.sample_id = s.sample_id
-				and s.sample_id in
-					(select sample_id 
-					from sample group 
-					by location_feature_nc
-					having max(date_gathered))""")
+# 	cur.execute("""select s.location_feature_nc,l.common_feature_name,l.feature_system,l.description,p.initialTemp, l.lat,l.lng
+# 				from sample s, physical_data p, location l 
+# 				where l.FEATURE_NC = s.location_feature_nc
+# 				and p.sample_id = s.sample_id
+# 				and s.sample_id in
+# 					(select sample_id 
+# 					from sample group 
+# 					by location_feature_nc
+# 					having max(date_gathered))""")
 	
-	positions = [dict(lat=float(row[5]),lng=row[6],city=row[2],  desc=row[3], feature_name=row[1],id=row[0]) for row in cur.fetchall()]
+# 	positions = [dict(lat=float(row[5]),lng=row[6],city=row[2],  desc=row[3], feature_name=row[1],id=row[0]) for row in cur.fetchall()]
 	
-	app.logger.debug(positions)
+# 	app.logger.debug(positions)
 	
-	cur.close()
+# 	cur.close()
 
-	return render_template('mapsearch.html',positions=positions)
+# 	return render_template('mapsearch.html',positions=positions)
 
 	
 @app.route('/simpleresults')
-def simpleresults():
-	
-	cur = g.db.cursor()	
+def simpleresults():	
 
 	minTemp = request.args.get('minTemp')
 	maxTemp = request.args.get('maxTemp')
@@ -202,95 +184,12 @@ def simpleresults():
 		
 		if(s.location_id != prev):
 			latestSamples.append(tempSamples[0])
-			tempSamples = []
-			
+			tempSamples = []			
 
 		tempSamples.append(s)
 		prev = s.location_id
 	
 	latestSamples.append(tempSamples[0])	
-	# latestSamples = sampleSites
-	# latestSamples = latestSamples.paginate(2,app.config['IMAGES_PER_PAGE'],False)
-	
-	# for s in sampleSites:		
-	# 	print s.location_id,s.date_gathered
-
-
-	# app.logger.debug(sampleSites.count())
-	# app.logger.debug(dir(sampleSites[0]))
-	# session['filter'] = filter
-	
-
-	# tempMin = 0
-
-	# tempMax = 100
-	
-	# query = """select s.location_feature_nc,l.common_feature_name,l.feature_system,l.description,p.initialTemp, l.lat,l.lng,l.toilet, l.parkbench,l.track, l.private, i.image_path
-	# 				from sample s, physical_data p, location l, images i
-	# 				where l.FEATURE_NC = s.location_feature_nc
-	# 				and p.sample_id = s.sample_id
-	# 				and i.sample_id = s.sample_id
-	# 				and s.sample_id =
- #       								( SELECT sample_id
- #           							FROM sample
-	# 								where location_feature_nc = s.location_feature_nc
-	# 								order by date_gathered desc
-	# 								limit 1
- #   									)"""
-
-	# if filter == "city":
-	# 	queryCity = request.args.get('city')
-	# 	query += "and l.feature_system = '{city}'".format(city=queryCity);
-				
-
-		
-	# 	filter = queryCity
-	# 	session['filter'] = queryCity
-	# elif filter.lower() == "safe":
-		
-	# 	tempMax = 60
-	# 	query += "and p.initialTemp <= {tempMax}".format(tempMax=tempMax)
-		
-						
-	# elif filter.lower() == "unsafe":
-	
-	# 	tempMin =60
-
-	# 	query += "and p.initialTemp >= {tempMin}".format(tempMin=tempMin)
-		
-		
-	
-	# elif filter.lower() == "hottest":
-	# 	tempMin = 90
-	# 	query += "and p.initialTemp >= {tempMin}".format(tempMin=tempMin)
-		
-	
-	# elif filter.lower() == "unique":
-	# 	site_id = "WAI_TPU"
-	# 	query += "and l.feature_nc = '{site_id}'".format(site_id=site_id)
-		
-		
-	# else:
-	# 	if request.args.get('tempMin') is not None:
-	# 		tempMin = request.args.get('tempMin')
-		
-	# 	if request.args.get('tempMax') is not None:
-	# 		tempMax = request.args.get('tempMax')
-
-		
-	# 	query += """and p.initialTemp >= {tempMin}
-	# 				and p.initialTemp <= {tempMax}""".format(tempMin=tempMin,tempMax=tempMax)
-		
-	
-	# temp = ""
-	# springAtt = request.args.getlist("items");
-	
-	# for att in springAtt:
-	# 	temp += "and " + att + "= true ";
-	
-	# query += temp
-	# app.logger.debug (app.config['SQLALCHEMY_DATABASE_URI'])
-	# cur.execute(query)
 
 	entries = [dict(location_id=s.location_id,
 					feature_name=s.location.feature_name, 
@@ -334,13 +233,12 @@ def simpleresults():
 	return render_template('simpleresults.html',entries=entries,
 												form=form,
 												minTemp=minTemp,
-												maxTemp=maxTemp,
-												
+												maxTemp=maxTemp,												
 												 pieChart=pieChart
 												)
 
 
-	# return render_template('about.html')
+
 @app.route('/searchbyimage')
 @app.route('/searchbyimage/<int:page>')
 @app.route('/searchbyimage/<showAll>')
@@ -414,7 +312,7 @@ def samplesite(site_id):
 
 @app.route("/sotd")
 def sotd():
-	sotdTup = GetSOTD()
+	# sotdTup = GetSOTD()
 	
 
 	return render_template('sotd.html',sotdimg = sotdTup[0],
@@ -428,39 +326,39 @@ def channel():
 def searchbycat():
 	return render_template('searchbycategory.html')
 
-def GetSOTD():
-	g.db = connect_db()
+# def GetSOTD():
+# 	g.db = connect_db()
 
-	cur = g.db.cursor()
+# 	cur = g.db.cursor()
 
-	f = open('SOTD.ini','r')
+# 	f = open('SOTD.ini','r')
 
 	
 
-	date = f.readline().rstrip()
-	springIndex = f.readline().rstrip()
-	springIndex = int(springIndex)
-	currentDate = datetime.datetime.strptime(date,"%d/%m/%Y")
+# 	date = f.readline().rstrip()
+# 	springIndex = f.readline().rstrip()
+# 	springIndex = int(springIndex)
+# 	currentDate = datetime.datetime.strptime(date,"%d/%m/%Y")
 
-	now = datetime.datetime.now()
+# 	now = datetime.datetime.now()
 
-	#now = now.strftime("%d/%m/%Y")
+# 	#now = now.strftime("%d/%m/%Y")
 
-	if (now - currentDate) > datetime.timedelta(days=1):
-		springIndex+=1
+# 	if (now - currentDate) > datetime.timedelta(days=1):
+# 		springIndex+=1
 
-	query = """select image_path
-				from images"""
+# 	query = """select image_path
+# 				from images"""
 
-	cur.execute(query)
-	results = cur.fetchall()
-	f.close()
-	f = open('SOTD.ini','w+')	
-	f.write(datetime.datetime.now().strftime("%d/%m/%Y") + "\n")
-	f.write(str(springIndex))
+# 	cur.execute(query)
+# 	results = cur.fetchall()
+# 	f.close()
+# 	f = open('SOTD.ini','w+')	
+# 	f.write(datetime.datetime.now().strftime("%d/%m/%Y") + "\n")
+# 	f.write(str(springIndex))
 	
-	f.close()
-	return results[springIndex]  
+# 	f.close()
+# 	return results[springIndex]  
 
 
 	
