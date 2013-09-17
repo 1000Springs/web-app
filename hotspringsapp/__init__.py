a3b7d78a1f37fe8577546092e2edaeb2670d27d6
Mathew Button <mathewdbutton@gmail.com>
2013-09-17 12:03:48 +1200

    db object created when app started
    instead of in models.py

--- hotspringsapp/__init__.py
+++ hotspringsapp/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy, Pagination
 
 app = Flask('hotspringsapp')
+
+db = SQLAlchemy(app)
 
 app.config.from_pyfile('hotsprings.cfg')
 
