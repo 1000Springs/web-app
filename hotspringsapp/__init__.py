37dfe3d4a124ad61eac716491f72a88215c7620e
Duncan White <D.White@gns.cri.nz>
2015-11-02 12:32:38 +1300

    Changed to use filesystem cache, as the simple cache only works properly on the dev server.

--- hotspringsapp/__init__.py
+++ hotspringsapp/__init__.py
@@ -10,7 +10,7 @@ app.config.from_pyfile('hotsprings.cfg')
 
 # Cache for storing data that changes infrequently
 # Store objects for a year by default(60 * 60 * 24 * 365 seconds)
-app.cache = Cache(app,config={'CACHE_TYPE': 'simple', 'CACHE_DEFAULT_TIMEOUT': 60 * 60 * 24 * 365})
+app.cache = Cache(app,config={'CACHE_TYPE': 'filesystem', 'CACHE_DIR': '/tmp/flask-cache', 'CACHE_DEFAULT_TIMEOUT': 60 * 60 * 24 * 365})
 app.initTaxonomyCacheThreads=0
 
 import hotspringsapp.views
